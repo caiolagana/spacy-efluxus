b54f643e44a50493e26a829480df92adf9457803
Caio Lagana <caiolagana@Caios-MacBook-Air.local>
2022-10-13 20:42:10 -0300

    primeiro teste com texto proprio

--- main.py
+++ main.py
@@ -5,3 +5,9 @@ doc = nlp(sentences[0])
 print(doc.text)
 for token in doc:
     print(token.text, token.pos_, token.dep_)
+
+
+file = open("input-test.txt", "r")
+f = nlp(file.read())
+for token in f:
+    print(token.text, token.pos_, token.dep_)
